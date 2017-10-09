37f82921a56eebd55036714f0645c3dc4219520e
Pawan Sasanka <pawan@duosoftware.com>
2017-10-09 07:40:39 +0530

    display call waiting time exceeded fixed

--- app/directive/dashboard/queueDetailsDirective.js
+++ app/directive/dashboard/queueDetailsDirective.js
@@ -6,109 +6,23 @@ agentApp.directive('queuedetails', function ($timeout) {
         },
         templateUrl: 'app/views/dashboard/partials/queueDetails.html',
         link: function (scope) {
-            scope.isNotified = false;
+
             scope.isExceeded=false;
-            scope.checkTimeExceed = function () {
 
 
-                if(scope.queue.QueueInfo.CurrentMaxWaitTime !=0 && scope.queue.queueDetails && scope.queue.queueDetails.MaxWaitTime)
+            scope.$on('timer-tick',function (e,data) {
+
+                //scope.checkTimeExceed();
+                if(data.millis && scope.queue.queueDetails && scope.queue.queueDetails.MaxWaitTime && data.millis >=(scope.queue.queueDetails.MaxWaitTime*1000) )
                 {
-                    var diffTime= moment().diff(moment(scope.queue.QueueInfo.CurrentMaxWaitTime),'seconds',true);
-                    if(diffTime>scope.queue.queueDetails.MaxWaitTime)
-                    {
-                        scope.isExceeded=true;
-                        scope.isNotified = true;
-                    }
-                    else
-                    {
-                        scope.isExceeded=false;
-                        scope.isNotified = false;
-                    }
-                }
-                else
-                {
-                    scope.isExceeded=false;
-                    scope.isNotified = false;
-                }
-
-
-
-
-
-              /*  if(scope.queue.QueueInfo.CurrentMaxWaitTime !=0  && scope.queue.QueueInfo.EventTime && scope.queue.queueDetails.MaxWaitTime)
-                {
-
-
-
-
-                    if (scope.queue.QueueInfo.CurrentMaxWaitTime) {
-
-                        var currWait =0;
-                        var d = moment(scope.queue.QueueInfo.CurrentMaxWaitTime).valueOf();
-
-                        if (scope.queue.QueueInfo.EventTime) {
-
-                            var serverTime = moment(scope.queue.QueueInfo.EventTime).valueOf();
-                            tempMaxWaitingMS = serverTime - d;
-
-                            var myTime = moment().valueOf();
-
-                            var timeDiff= myTime-tempMaxWaitingMS;
-
-
-
-                            if(tempMaxWaitingMS==0)
-                            {
-                                currWait=myTime-d;
-                            }
-                            else
-                            {
-                                currWait=timeDiff;
-                            }
-
-                            if(currWait>(scope.queue.queueDetails.MaxWaitTime*1000))
-                            {
-                                scope.isExceeded=true;
-                                scope.isNotified = true;
-                            }
-                            else
-                            {
-                                scope.isExceeded=false;
-                                scope.isNotified = false;
-                            }
-
-                        }
-                    }
-
-
-                    /!* var curMaxTime =  moment(scope.queue.QueueInfo.CurrentMaxWaitTime);
-                     var curTime =moment(moment.now());
-                     var diffTime = curTime.diff(curMaxTime);
-                     if(diffTime>(scope.queue.queueDetails.MaxWaitTime*1000))
-                     {
-                     scope.isExceeded=true;
-                     scope.isNotified = true;
-                     }
-                     else
-                     {
-                     scope.isExceeded=false;
-                     scope.isNotified = false;
-                     }*!/
-
-
+                    scope.isExceeded=true;
 
                 }
                 else
                 {
                     scope.isExceeded=false;
-                    scope.isNotified = false;
-                }*/
-            }
-            scope.checkTimeExceed();
 
-            scope.$on('timer-tick',function (e) {
-
-                scope.checkTimeExceed();
+                }
 
             });
 
